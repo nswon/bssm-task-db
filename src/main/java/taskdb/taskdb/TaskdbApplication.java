80c6aaadfc2e5284b35256d4a7e25a9ad0ec5b7c
nswon <swnam0729@gmail.com>
2022-11-01 12:23:22 +0900

    init: 프로젝트 빌드

---
+++ src/main/java/taskdb/taskdb/TaskdbApplication.java
@@ -0,0 +1,13 @@
+package taskdb.taskdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/taskdb/taskdb/TaskdbApplicationTests.java
@@ -0,0 +1,13 @@
+package taskdb.taskdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
