cb91f5ae4bbdb4a211d566b8608905b0c03d7e00
nswon <swnam0729@gmail.com>
2022-11-24 05:52:59 +0900

    feat: 질문글 전체조회시 카테고리 포함

--- src/main/java/taskdb/taskdb/domain/questions/presentation/dto/response/QuestionsResponseDto.java
+++ src/main/java/taskdb/taskdb/domain/questions/presentation/dto/response/QuestionsResponseDto.java
@@ -2,6 +2,7 @@ package taskdb.taskdb.domain.questions.presentation.dto.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import taskdb.taskdb.domain.questions.domain.Category;
 import taskdb.taskdb.domain.questions.domain.Question;
 import taskdb.taskdb.domain.questions.domain.QuestionStatus;
 
@@ -10,16 +11,16 @@ public class QuestionsResponseDto {
     private final Long id;
     private final QuestionStatus status;
     private final String title;
-    //카테고리
+    private final Category category;
     //댓글 개수
     //좋아요 개수
     //등록 날짜
-    //카테고리
 
     @Builder
     public QuestionsResponseDto(Question question) {
         this.id = question.getId();
         this.status = question.getQuestionStatus();
         this.title = question.getTitle();
+        this.category = question.getCategory();
     }
 }
